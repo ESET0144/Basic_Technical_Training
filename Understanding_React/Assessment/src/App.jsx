fbd272ceafbee3e3dc8b02745314441e280dd32f
ESET0144 <Udaykumar.vadecha@esyasoft.com>
2025-09-09 18:38:20 +0530

    React Assessment completed

---
+++ Understanding_React/Assessment/src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+const Button = ({label, onClick}) => {
+  return <button onClick={onClick}>{label}</button>;
+}
+
+
+  return(
+    <div> 
+      <h2 id="a"> 0 </h2>
+      <Button 
+      label = "Increment"
+      onClick = {() =>{
+        const element = document.getElementById("a");
+        var curr = parseInt(element.textContent)
+        element.textContent = curr + 1;
+        console.log("Increment clicked")
+      }
+      
+      }/>
+
+
+      <Button 
+      label = "Decrement"
+      onClick = {() =>{
+        const element = document.getElementById("a");
+        var curr = parseInt(element.textContent)
+        element.textContent = curr - 1;
+        console.log("Decrement clicked")
+      }
+      
+      }/>
+    </div>
+  )
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://www.youtube.com/" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://www.youtube.com/" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+}
+
+// export default App
+export default App
