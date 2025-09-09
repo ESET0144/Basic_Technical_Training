29e5cc800dde8b1bc6d53c2c0f7f8c02e2aee17b
ESET0144 <Udaykumar.vadecha@esyasoft.com>
2025-09-09 11:35:54 +0530

    Rename of all parent folders

---
+++ Basics_of_Embedded_Systems/Day-6_09-09/Project_6/src/main.c
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+// set PD4 as output
+#define DATA_PIND7 PD7
+#define DATA_PIND6 PB0
+#define DATA_PIND5 PB1
+#define DATA_PIND4 PB2
+#define ENABLE PB3
+#define DATA_RS PB4
+
+int lcd_command(unsigned int cmd) {
+    
+}
+
+int main()
+{
+
+    
+}
