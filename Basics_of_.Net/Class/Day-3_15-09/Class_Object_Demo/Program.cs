3c1ab3fb5296582d70fa27c77b9ecbcade14d4cd
ESET0144 <Udaykumar.vadecha@esyasoft.com>
2025-09-16 09:08:54 +0530

    Python and .net files added

---
+++ Basics_of_.Net/Class/Day-3_15-09/Class_Object_Demo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Class_Object_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student first = new student(1, "Uday", "01/01/2020", "abc@gmail.com");
+            first.showDisplay();
+            first.initialize();
+            first.student_name = "Raj";
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Basics_of_.Net/Class/Day-3_15-09/Class_Object_Demo/student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Object_Demo
+{
+    internal class student
+    {
+        private int student_id;
+        public string student_name;
+        private string DOB;
+        string emailid;
+
+
+        public student() // default constructor to manage null object of student
+        {
+
+        }
+        public student(int student_id, string student_name, string dOB, string emailid) // parameterized constructor
+        {
+            this.student_id = student_id;
+            this.student_name = student_name;
+            DOB = dOB;
+            this.emailid = emailid;
+        }
+
+        public void initialize()
+        {
+            student_id = 10;
+            student_name = "Uday";
+        }
+        public void showDisplay()
+        {
+            Console.WriteLine("Display Object L");
+            Console.WriteLine(student_name);
+        }
+        public void Avg_marks()
+        {
+
+        }
+
+        public void Avg_age()
+        {
+
+        }
+
+        public void Avg_height()
+        {
+
+        }
+    }
+}
