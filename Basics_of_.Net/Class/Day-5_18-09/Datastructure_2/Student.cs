bb05ad092514fb2a2323e97ffb4c8b1019464fe2
ESET0144 <Udaykumar.vadecha@esyasoft.com>
2025-09-18 15:23:41 +0530

    Datastructure_2 file, class assignment completed

---
+++ Basics_of_.Net/Class/Day-5_18-09/Datastructure_2/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructure_2
+{
+    internal class Student
+    {
+        public int id;
+        public int marks;
+        public string name;
+
+        public Student(int id, string name, int marks) // parameterized constructor
+        {
+            this.id = id;
+            this.name = name;
+            this.marks = marks;
+        }
+
+        public Student() // default constructor
+        {
+
+        }
+    }
+}
